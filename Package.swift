4e556ff92a870919268ee28853bf120abe09f106
heavenlycreature <lepiinnn@gmail.com>
2026-08-06 23:58:36 +0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:6.3
+import PackageDescription
+
+let package = Package(
+    name: "MangoMonitorDashboardApi",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.121.4"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.101.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "MangoMonitorDashboardApi",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "MangoMonitorDashboardApiTests",
+            dependencies: [
+                .target(name: "MangoMonitorDashboardApi"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("InternalImportsByDefault"),
+    .enableUpcomingFeature("MemberImportVisibility"),
+    .enableUpcomingFeature("InferIsolatedConformances"),
+    .enableUpcomingFeature("ImmutableWeakCaptures"),
+] }
---
+++ Sources/MangoMonitorDashboardApi/configure.swift
@@ -0,0 +1,10 @@
+import Vapor
+
+/// configures your application
+func configure(_ app: Application) async throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // register routes
+    try routes(app)
+}
